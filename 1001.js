973513b244038ac0ac426b9624ec931ff4bf2072
YuriDrTi <71226673+YuriDrTi@users.noreply.github.com>
2023-06-29 14:29:40 -0300

    1000a1001

---
+++ 1001.js
@@ -0,0 +1,11 @@
+//var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+//var lines = input.split('\n');
+
+
+
+var A = Number("10");
+var B = Number("9");
+var X = (A + B);
+
+
+console.log(`X = ${X}`)//`server para escrever texto e ${} imprimir valor
