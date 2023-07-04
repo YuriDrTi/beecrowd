31310b683ea87351b252522a910e709eb9920d38
YuriDrTi <71226673+YuriDrTi@users.noreply.github.com>
2023-07-04 15:44:09 -0300

    1016

---
+++ 1016.js
@@ -0,0 +1,15 @@
+//var input = require('fs').readFileSync('entrada','utf8');
+//var lines = input.split('\n');
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var R = (Number(lines[0]) * 2);
+
+console.log(`${R} minutos`);
+
+//var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+//var lines = input.split(' ');
+
+//var R = (lines * 2);
+
+//console.log(`${R} minutos`);
